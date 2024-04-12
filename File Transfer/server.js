b630b2f5d437875a901955b20350a8b5a3e191f9
ishita-1609 <ishitamoorjani@gmail.com>
2024-04-12 21:46:41 +0530

    server

--- File Transfer/server.js
+++ File Transfer/server.js
@@ -9,6 +9,29 @@ var mongodb = require("mongodb");
 var mongoClient = mongodb.MongoClient;
 var ObjectId = mongodb.ObjectId;
 
+app.set("view engine" , "ejs") ;
+
+app.use("/public/css" , express.static(__dirname + "/public/css")) ;
+app.use("/public/js" , express.static(__dirname + "/public/js")) ;
+app.use("/public/font-awesome-4.7.0" , express.static(__dirname + "/public/font-awesome-4.7.0")) ;
+
+var session = require("express-session") ;
+app.use(session({
+    secret: "secret key" ,
+    resave: false ,
+    saveUninitialized: false
+}));
+
+app.use(function(request , result , next){
+    request.mainURL = mainURL ;
+    request.isLogin = (typeof request.session.user !== "undefined") ;
+    request.user = request.session.user ;
+
+    next() ;
+});
+
+
+
 http.listen(3000, function() {
     console.log("Server started at " + mainURL);
 
@@ -18,6 +41,12 @@ http.listen(3000, function() {
         } else {
             database = client.db("file_transfer");
             console.log("Database connected");
-        }
+        
+        app.get("/" , function(request , result) {
+            result.render("index" , {
+                "request": request
+            });
+        });
+    }
     });
 });
